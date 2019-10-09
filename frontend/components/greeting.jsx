7f76490b218f5bff675e9637919fff93d9b27c7c
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-10-08 21:02:03 -0500

    make it better

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Greeting = props => {
-  let header;
-  if (props.currentUser) {
+class Greeting extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    document.getElementById("session_state").classList.toggle("show");
+  }
+
+  render() {
+    let header;
+    if (this.props.currentUser) {
+      header = (
+        <button className='current_user' onClick={this.props.logout}>Logout</button>
+      )
+    } else {
+      header = (
+        <div className='no_current_user'>
+          <Link to="/signup">Signup</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      )
+    }
+
     return (
-      <button className='current_user' onClick={props.logout}>Logout</button>
-      // <div className='current_user'>
-      //   <h3>{`Welcome ${props.currentUser.username}!`}</h3>
-      // </div>
-    )
-  } else {
-    return (
-      <div className='no_current_user'>
-        <Link to="/signup">Signup</Link>
-        <Link to="/login">Login</Link>
+      <div className="dropdown">
+        <button onClick={this.handleClick} className="dropdownbtn">Button</button>
+        <div id="session_state" className="dropdown-content">
+          {header}
+        </div>
       </div>
     )
   }
-
-  // return (
-  //   <div>
-  //     {header}
-  //   </div>
-  // )
 }
 
 export default Greeting;
